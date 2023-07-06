f3f8d58c9a2bd8d855126c9643269295a9e0bf2c
wenkrang <2934356978@qq.com>
2023-07-06 16:00:53 +0800

    新增命令 kf

---
+++ src/main/java/com/wenkrang/kitfood/kf.java
@@ -0,0 +1,13 @@
+package com.wenkrang.kitfood;
+
+import org.bukkit.block.CommandBlock;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class kf implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return true;
+    }
+}

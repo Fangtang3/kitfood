219bb57154b65ca5f4957a926721abdc83a2ed10
wenkrang <2934356978@qq.com>
2023-07-06 16:13:12 +0800

    更新

--- src/main/java/com/wenkrang/kitfood/Kitfood.java
+++ src/main/java/com/wenkrang/kitfood/Kitfood.java
@@ -7,7 +7,7 @@ public final class Kitfood extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        this.getCommand("kf").setExecutor(new kf());
     }
 
     @Override
--- src/main/java/com/wenkrang/kitfood/kf.java
+++ src/main/java/com/wenkrang/kitfood/kf.java
@@ -4,10 +4,14 @@ import org.bukkit.block.CommandBlock;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class kf implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args[0].equalsIgnoreCase("help")) {
+            sender.sendMessage("");
+        }
         return true;
     }
 }
